7decd37e8614cb09f73172c4a9c746ef75e3f294
Sourav Das <souravdas4816@gmail.com>
2023-12-04 19:29:58 +0530

    Ternary operator

--- 02-A-First-Look-at-React/04-Essential-JavaScript-for-React/script.js
+++ 02-A-First-Look-at-React/04-Essential-JavaScript-for-React/script.js
@@ -178,5 +178,10 @@ updatedBook;
 // Template Literals
 const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
   publicationDate.split('-')[0]
-}`;
+}. The book has ${hasMovieAdaptation ? '' : 'not'} been adapted as a movie`;
 summary;
+
+// Ternary operator
+const pagesRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
