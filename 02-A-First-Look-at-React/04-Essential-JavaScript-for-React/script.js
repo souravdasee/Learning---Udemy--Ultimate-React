0985b89e5b0357253465040628e011c751944845
Sourav Das <souravdas4816@gmail.com>
2023-12-04 20:49:47 +0530

    Optional Chaining

--- 02-A-First-Look-at-React/04-Essential-JavaScript-for-React/script.js
+++ 02-A-First-Look-at-React/04-Essential-JavaScript-for-React/script.js
@@ -217,3 +217,13 @@ countWrong;
 
 const count = book.reviews.librarything.reviewsCount ?? 'no data';
 count;
+
+// Optional Chaining
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews?.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  librarything;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book));
