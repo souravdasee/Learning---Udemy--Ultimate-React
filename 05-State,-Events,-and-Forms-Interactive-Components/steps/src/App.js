5291c4fae8d6255fe396afe319e2d6506d86ffb7
Sourav Das <souravdas4816@gmail.com>
2023-12-07 19:19:22 +0530

    Creating a State Variable With useState

--- 05-State,-Events,-and-Forms-Interactive-Components/steps/src/App.js
+++ 05-State,-Events,-and-Forms-Interactive-Components/steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,22 +7,22 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert('Previous');
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
-    alert('Next');
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className="message">
@@ -29,13 +31,13 @@ export default function App() {
 
       <div className="buttons">
         <button
-          style={{ backgroundColor: '#7950f2', color: 'fff' }}
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
           onClick={handlePrevious}
         >
           Previous
         </button>
         <button
-          style={{ backgroundColor: '#7950f2', color: 'fff' }}
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
           onClick={handleNext}
         >
           Next
